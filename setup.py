63b21e74a0ed665595d81c895b82c0de8ef7b7c2
Jeff Wu <wuthefwasthat@gmail.com>
2020-06-27 12:10:21 -0700

    small updates to setup

--- setup.py
+++ setup.py
@@ -4,8 +4,10 @@ setup(
     name="tapestry",
     py_modules=["tapestry"],
     version="0.0.1",
+    author="Jeffrey Wu",
     install_requires=[
     ],
+    url="https://github.com/wuthefwasthat/tapestry",
     description="A redux-saga inspired effects library",
-    author="Jeffrey Wu",
+    python_requires='>=3.7',
 )

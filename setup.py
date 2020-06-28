9c1d866b7ca880e64954901dd5d63de9c7a0eb2d
Jeff Wu <wuthefwasthat@gmail.com>
2020-06-27 18:42:27 -0700

    version 0.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="tapystry",
     packages=["tapystry"],
-    version="0.0.4",
+    version="0.1.0",
     author="Jeffrey Wu",
     install_requires=[
     ],

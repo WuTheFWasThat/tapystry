9a66ef3f3b9553dc2db217276b96ea86360291c4
Jeff Wu <wuthefwasthat@gmail.com>
2021-12-01 15:34:53 -0800

    bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="tapystry",
     packages=["tapystry"],
-    version="0.3.22",
+    version="0.4.0",
     author="Jeffrey Wu",
     install_requires=[
     ],
